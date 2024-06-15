04b7cbab57bd8fac8567a5037dc5ca7c1a94b406
Mahmoud Khalid <103892504+MahmoudKhalid22@users.noreply.github.com>
2024-06-14 17:34:46 -0700

    chore: add overlay functionality

--- todo/src/components/Add.tsx
+++ todo/src/components/Add.tsx
@@ -2,15 +2,40 @@ import { useState } from "react";
 import Modal from "./Modal";
 
 const Add = () => {
-  const [modal, setModal] = useState(false);
+  const [modal, setModal] = useState<boolean>(false);
 
   return (
     <div>
-      {modal &&
-      
-      <Modal />
-      
-      }
+      {modal && <Modal onSetModal={setModal} />}
+
+      {modal && (
+        <form className="relative h-full block mx-auto w-fit z-50">
+          <div className="w-fit h-fit gap-4 bg-[#ececec] px-6 py-10 rounded-lg ">
+            <label className="text-lg mb-4 block cursor-pointer" htmlFor="todo">
+              Add Todo
+            </label>
+            <input
+              placeholder="todo"
+              className="w-full px-2 py-4 text-xl rounded-lg mb-6"
+              id="todo"
+            />
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 transition-colors text-white w-full"
+            >
+              Add
+            </button>
+            <button
+              type="button"
+              className="bg-red-500 hover:bg-red-700 transition-colors text-white w-full mt-6"
+              onClick={() => setModal(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      )}
+
       <button
         onClick={() => setModal(true)}
         className="absolute right-16 bottom-16 flex justify-center items-center 
