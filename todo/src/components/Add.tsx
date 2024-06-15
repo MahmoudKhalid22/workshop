10d90e5fe1e5af44e5e11c02c5946d107c84fc5b
Mahmoud Khalid <103892504+MahmoudKhalid22@users.noreply.github.com>
2024-06-14 17:42:58 -0700

    chore: responsive design

--- todo/src/components/Add.tsx
+++ todo/src/components/Add.tsx
@@ -38,9 +38,9 @@ const Add = () => {
 
       <button
         onClick={() => setModal(true)}
-        className="absolute right-16 bottom-16 flex justify-center items-center 
-    text-4xl rounded-full
-    w-20 h-20 bg-blue-500 text-white font-semibold  shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+        className="absolute md:right-16 md:bottom-16 right-4 bottom-4 flex justify-center items-center 
+    md:text-4xl text-xl rounded-full
+    md:w-20 md:h-20 w-8 h-12 bg-blue-500 text-white font-semibold  shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
       >
         +{" "}
       </button>
--- todo/src/components/Header.tsx
+++ todo/src/components/Header.tsx
@@ -1,20 +1,22 @@
 function Header() {
   return (
     <div className="my-4">
-      <h2 className="text-2xl font-normal my-8">TODO LIST</h2>
-      <div className="flex items-center gap-4 justify-center w-full">
+      <h2 className="text-md md:text-2xl font-normal my-2 md:my-8">
+        TODO LIST
+      </h2>
+      <div className="flex flex-col md:flex-row items-center gap-4 justify-center w-full">
         <form className="w-full">
-          <div className="relative overflow-hidden border border-blue-600 rounded-lg px-4 py-2 flex items-center gap-6 justify-between">
+          <div className="relative overflow-hidden border border-blue-600 rounded-lg px-2 md:px-4 py-2 flex items-center gap-6 justify-between">
             <input
               placeholder="Search Todo"
-              className="border-none outline-none text-2xl w-full"
+              className="border-none outline-none text-md md:text-2xl w-full"
             />
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="1em"
               height="1em"
               viewBox="0 0 24 24"
-              className="absolute right-0  text-2xl h-12 w-12 bg-blue-600 hover:bg-blue-800 transition-colors text-[#ececec] cursor-pointer px-2"
+              className="absolute right-0  md:text-2xl text-sm h-12 w-8 bg-blue-600 hover:bg-blue-800 transition-colors text-[#ececec] cursor-pointer px-2"
             >
               <path
                 fill="currentColor"
